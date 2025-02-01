3c38f8188261b3e65e934cddf4b05b6041691c0b
Egor Makarenko <egormkn@yandex.ru>
2025-02-01 19:29:20 +0000

    ci: add prettier plugin for sorting imports
    Command: `npm i -D @trivago/prettier-plugin-sort-imports`

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import("prettier").Config} */
 const config = {
-  "plugins": [
+  plugins: [
+    "@trivago/prettier-plugin-sort-imports",
     "prettier-plugin-tailwindcss" // MUST come last
-  ]
+  ],
+  importOrder: ["^@/(.*)$", "^[./].*(?<!\\.(?:c|sc|sa)ss)$", "^[./].*(?<=\\.(?:c|sc|sa)ss)$"],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
 };
 
 export default config;
