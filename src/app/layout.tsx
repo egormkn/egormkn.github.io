d48edb613006fc37a254424bef58b1766fba88b6
Egor Makarenko <egormkn@yandex.ru>
2025-02-01 20:46:49 +0000

    refactor: extract fonts configuration to fonts.ts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,8 @@
-import { config } from "@fortawesome/fontawesome-svg-core";
-
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+
+import { geistMono, geistSans } from "./fonts";
+
 import "@/styles/globals.css";
-
-config.autoAddCss = false;
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
