707e0d1eb34c7c61700c2ce559006b4ccabb0357
Egor Makarenko <egormkn@yandex.ru>
2025-02-01 13:56:07 +0000

    ci: add Stylelint
    Command: `npm i -D stylelint stylelint-config-standard`

---
+++ stylelint.config.mjs
@@ -0,0 +1,25 @@
+const tailwindAtRules = [
+  "apply",
+  "config",
+  "custom-variant",
+  "import",
+  "plugin",
+  "reference",
+  "source",
+  "theme",
+  "utility",
+  "variant",
+];
+
+/** @type {import('stylelint').Config} */
+const stylelintConfig = {
+  extends: ["stylelint-config-standard"],
+  rules: {
+    "at-rule-no-unknown": [
+      true,
+      { ignoreAtRules: [ ...tailwindAtRules ] },
+    ],
+  },
+};
+
+export default stylelintConfig;
