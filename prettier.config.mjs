3d344be43f7958aa3a3a232af6cdc48c8094198a
Egor Makarenko <egormkn@yandex.ru>
2026-06-15 13:05:24 +0500

    ci: add prettier plugin for TailwindCSS
    Command: `npm i -D prettier-plugin-tailwindcss`

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,4 +1,7 @@
-/** @type {import("prettier").Config} */
-const config = {};
+/** @type {import("prettier").Config & import('prettier-plugin-tailwindcss').PluginOptions} */
+const config = {
+  plugins: ["prettier-plugin-tailwindcss"],
+  tailwindStylesheet: "./src/app/globals.css"
+};
 
 export default config;
